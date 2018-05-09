7fe47f8b7c1a8050dee469b292209b9afd478951
g4bleb <gableb@hotmail.fr>
2018-05-09 02:23:04 +0200

    Updated Qt to 5.10.1

--- miniprojetCPP/moc_Scene.cpp
+++ miniprojetCPP/moc_Scene.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'Scene.hpp'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.5.1)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.10.1)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -12,12 +12,14 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'Scene.hpp' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.5.1. It"
+#error "This file was generated using the moc from 5.10.1. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
 
 QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_Scene_t {
     QByteArrayData data[3];
     char stringdata0[12];
@@ -73,9 +75,9 @@ void Scene::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void 
     Q_UNUSED(_a);
 }
 
-const QMetaObject Scene::staticMetaObject = {
+QT_INIT_METAOBJECT const QMetaObject Scene::staticMetaObject = {
     { &QGraphicsScene::staticMetaObject, qt_meta_stringdata_Scene.data,
-      qt_meta_data_Scene,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+      qt_meta_data_Scene,  qt_static_metacall, nullptr, nullptr}
 };
 
 
@@ -86,9 +88,9 @@ const QMetaObject *Scene::metaObject() const
 
 void *Scene::qt_metacast(const char *_clname)
 {
-    if (!_clname) return Q_NULLPTR;
+    if (!_clname) return nullptr;
     if (!strcmp(_clname, qt_meta_stringdata_Scene.stringdata0))
-        return static_cast<void*>(const_cast< Scene*>(this));
+        return static_cast<void*>(this);
     return QGraphicsScene::qt_metacast(_clname);
 }
 
@@ -108,4 +110,5 @@ int Scene::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     }
     return _id;
 }
+QT_WARNING_POP
 QT_END_MOC_NAMESPACE
